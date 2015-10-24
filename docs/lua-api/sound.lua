16b02f1bc2e4f45018ff5d949593d0c3c33bac33
Johannes Pollitt <johannes@pollitt.de>
2015-10-24 15:40:20 +0200

    Add luadoc.

--- docs/lua-api/sound.lua
+++ docs/lua-api/sound.lua
@@ -1,1 +1,10 @@
-function play(players, sound)
+--- Play a sound to a specific player.
+-- @param player player who listen to the sound.
+-- @param sound sound which will playing.
+function playSound(player, sound) end
+
+--- Play a note to a specific player.
+-- @param player player who listen to the note.
+-- @param instrument instrument which will playing.
+-- @param note note which will playing.
+function playNote(player, instrument, note) end
