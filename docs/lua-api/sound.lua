ffa611a924da15e527ede4cf5f9624e43932ff53
Maik Marschner <maik@maik.tk>
2015-10-24 18:09:39 +0200

    Fix spelling in luadoc.

--- docs/lua-api/sound.lua
+++ docs/lua-api/sound.lua
@@ -1,10 +1,10 @@
 --- Play a sound to a specific player.
--- @param player player who listen to the sound.
--- @param sound sound which will playing.
+-- @param player player to play the sound to
+-- @param sound sound to play
 function playSound(player, sound) end
 
 --- Play a note to a specific player.
--- @param player player who listen to the note.
--- @param instrument instrument which will playing.
--- @param note note which will playing.
+-- @param player player to play the note to
+-- @param instrument instrument which will playing
+-- @param note note to play
 function playNote(player, instrument, note) end
