3b279e5650b9c27edaedb1ad1bff87df7ffb57f3
leMaik <an@maik.tk>
2015-10-30 00:31:26 +0100

    Remove return value for trading open function in luadoc.

--- docs/lua-api/trading.lua
+++ docs/lua-api/trading.lua
@@ -5,6 +5,5 @@
 -- @param player player to show the menu to
 -- @param offers offers to show, each offer is an array of the form `[take, take, give]` or `[take, give]` where `take`
 -- and `give` are strings of the form `id:data:amount`.
--- @return a new function
 --
 function open(player, offers) end
