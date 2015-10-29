984f14be2838ec4909c11ed8c86fbcb42f31e7de
leMaik <an@maik.tk>
2015-10-29 01:39:35 +0100

    Add a respond function as second parameter in the ask callback. Add a lua function to tell the player something in a dialog.

--- src/main/java/de/craften/plugins/rpgplus/scripting/api/Dialogs.java
+++ src/main/java/de/craften/plugins/rpgplus/scripting/api/Dialogs.java
@@ -5,34 +5,49 @@ import de.craften.plugins.rpgplus.components.dialogs.AnswerHandler;
 import de.craften.plugins.rpgplus.scripting.util.ScriptUtil;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.luaj.vm2.LuaFunction;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
+import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.ThreeArgFunction;
 import org.luaj.vm2.lib.VarArgFunction;
 import org.luaj.vm2.lib.jse.CoerceLuaToJava;
-
-import java.awt.*;
 
 /**
  * Provides an API for dialogs.
  */
 public class Dialogs extends LuaTable {
+
     public Dialogs(final RpgPlus plugin) {
         set("ask", new VarArgFunction() {
             @Override
             public Varargs invoke(Varargs varargs) {
-                Entity entity = (Entity) CoerceLuaToJava.coerce(varargs.arg(1), Entity.class);
+                final Entity entity = (Entity) CoerceLuaToJava.coerce(varargs.arg(1), Entity.class);
                 Player player = ScriptUtil.getPlayer(varargs.arg(2));
                 final LuaFunction callback = varargs.checkfunction(4);
                 plugin.getDialogs().ask(entity, player, varargs.checkjstring(3), new AnswerHandler() {
                     @Override
-                    public void handleAnswer(Player player, String answer) {
-                        callback.invoke(LuaValue.valueOf(answer));
+                    public void handleAnswer(final Player player, String answer) {
+                        callback.invoke(LuaValue.valueOf(answer), new OneArgFunction() {
+                            @Override
+                            public LuaValue call(LuaValue message) {
+                                plugin.getDialogs().tell(entity, player, message.checkjstring());
+                                return LuaValue.NIL;
+                            }
+                        });
                     }
                 });
+                return LuaValue.NIL;
+            }
+        });
+
+        set("tell", new ThreeArgFunction() {
+            @Override
+            public LuaValue call(LuaValue luaEntity, LuaValue luaPlayer, LuaValue message) {
+                Entity entity = (Entity) CoerceLuaToJava.coerce(luaEntity, Entity.class);
+                Player player = ScriptUtil.getPlayer(luaPlayer);
+                plugin.getDialogs().tell(entity, player, message.checkjstring());
                 return LuaValue.NIL;
             }
         });
