5078bb33812fbf44e5097e02ae20be9f608ae7ce
leMaik <an@maik.tk>
2015-10-30 21:38:50 +0100

    Add convenient wrapping function to EntityWrapper.

--- src/main/java/de/craften/plugins/rpgplus/scripting/api/entities/EntityWrapper.java
+++ src/main/java/de/craften/plugins/rpgplus/scripting/api/entities/EntityWrapper.java
@@ -1,6 +1,8 @@
 package de.craften.plugins.rpgplus.scripting.api.entities;
 
 import de.craften.plugins.rpgplus.components.entitymanager.ManagedEntity;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 
 /**
  * A lua wrapper for a {@link de.craften.plugins.rpgplus.components.entitymanager.ManagedEntity}.
@@ -10,5 +12,10 @@ public class EntityWrapper {
 
     public EntityWrapper(ManagedEntity entity) {
         this.entity = entity;
+
+    }
+
+    public static LuaValue wrap(ManagedEntity entity) {
+        return CoerceJavaToLua.coerce(new EntityWrapper(entity));
     }
 }
