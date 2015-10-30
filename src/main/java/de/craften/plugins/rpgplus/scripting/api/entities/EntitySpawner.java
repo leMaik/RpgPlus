5381a39f12ea3062fd147fd7399822dfd610abaa
leMaik <an@maik.tk>
2015-10-30 22:07:58 +0100

    Rename entity register function and immediately spawn the entity.

--- src/main/java/de/craften/plugins/rpgplus/scripting/api/entities/EntitySpawner.java
+++ src/main/java/de/craften/plugins/rpgplus/scripting/api/entities/EntitySpawner.java
@@ -1,43 +1,40 @@
 package de.craften.plugins.rpgplus.scripting.api.entities;
 
+import de.craften.plugins.rpgplus.components.entitymanager.BasicManagedEntity;
+import de.craften.plugins.rpgplus.components.entitymanager.ManagedEntity;
+import de.craften.plugins.rpgplus.components.entitymanager.MovementType;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.TwoArgFunction;
 
-import de.craften.plugins.rpgplus.components.entitymanager.BasicManagedEntity;
-import de.craften.plugins.rpgplus.components.entitymanager.ManagedEntity;
-import de.craften.plugins.rpgplus.components.entitymanager.MovementType;
-
+/**
+ * Lua API for spawning entities.
+ */
 public class EntitySpawner {
+    public void installOn(LuaTable object) {
+        object.set("spawn", new TwoArgFunction() {
 
-    public void installOn(LuaTable object){
-        object.set("registerEntity", new TwoArgFunction() {
-            
             @Override
             public LuaValue call(LuaValue entityType, LuaValue optionsArg) {
-                
                 EntityType type = EntityType.valueOf(entityType.checkjstring());
-                Class<? extends Entity> entityClass = type.getEntityClass();
-                
                 LuaTable options = optionsArg.checktable();
-                
-                ManagedEntity<? extends Entity> entity = new BasicManagedEntity<>(entityClass, new Location(
+
+                ManagedEntity entity = new BasicManagedEntity<>(type.getEntityClass(), new Location(
                         Bukkit.getWorld(options.get("world").checkjstring()),
                         options.get("x").checkdouble(),
                         options.get("y").checkdouble(),
                         options.get("z").checkdouble()));
-                
+
                 entity.setName(options.get("name").optjstring(""));
                 entity.setIsTakingDamage(options.get("invulnerable").optboolean(false));
                 entity.setMovementType(MovementType.valueOf(options.get("movementType").optjstring("LOCAL")));
-                
+                entity.spawn();
+
                 return EntityWrapper.wrap(entity);
             }
         });
     }
-    
 }
