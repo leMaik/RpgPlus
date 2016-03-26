42879bf4d91dcb33c9f642b54ca90d550644c3e5
Johannes Pollitt <johannes@pollitt.de>
2016-03-26 22:49:58 +0100

    Add function to open a virtual inventory.

--- src/main/java/de/craften/plugins/rpgplus/scripting/api/InventoryModule.java
+++ src/main/java/de/craften/plugins/rpgplus/scripting/api/InventoryModule.java
@@ -1,16 +1,29 @@
 package de.craften.plugins.rpgplus.scripting.api;
 
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.luaj.vm2.LuaBoolean;
+import org.luaj.vm2.LuaInteger;
+import org.luaj.vm2.LuaTable;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.Varargs;
+
 import de.craften.plugins.rpgplus.components.inventory.ItemMatcher;
 import de.craften.plugins.rpgplus.scripting.ScriptingModule;
 import de.craften.plugins.rpgplus.scripting.util.ScriptUtil;
 import de.craften.plugins.rpgplus.scripting.util.luaify.LuaFunction;
 import de.craften.plugins.rpgplus.scripting.util.luaify.Luaify;
-import org.luaj.vm2.*;
 
 /**
  * Lua API for the player's inventory.
  */
 public class InventoryModule extends LuaTable implements ScriptingModule {
+	
+	
     public InventoryModule() {
         Luaify.convertInPlace(this);
     }
@@ -49,7 +62,29 @@ public class InventoryModule extends LuaTable implements ScriptingModule {
 
         return LuaValue.varargsOf(missingAmounts);
     }
-
+    
+    @LuaFunction("openChest")
+    public Varargs openChest(Varargs args) {
+    	
+    	Player p = ScriptUtil.getPlayer(args.arg(1));
+       	int slots = args.arg(2).optint(1)*9;
+       	String title = ChatColor.translateAlternateColorCodes('&', args.arg(3).optjstring(""));
+       	Inventory inv = Bukkit.createInventory(null, slots, title);
+       	LuaTable itemTable = args.arg(4).opttable(null);
+       	if (itemTable != null) {
+       		for (int i = 0; i < slots; i++) {
+       			LuaValue value = itemTable.get(i);
+       			if (value != LuaValue.NIL) {
+       				ItemStack item = ScriptUtil.createItemMatcher(value).getItemStack();
+           			inv.setItem(i, item);
+       			}
+       		}
+        }
+       	p.openInventory(inv);
+    
+    	return LuaValue.NIL;
+    }
+    
     private static LuaBoolean checkItem(LuaValue player, LuaValue item) {
         ItemMatcher matcher = ScriptUtil.createItemMatcher(item);
         return LuaValue.valueOf(matcher.matches(ScriptUtil.getPlayer(player).getInventory()));
