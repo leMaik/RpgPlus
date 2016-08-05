3b55e478a2c9aa4533fe53d367927549703eb6cb
Johannes Pollitt <johannes@pollitt.de>
2016-08-05 23:22:46 +0200

    Add link to javadoc of the effect enum.

--- docs/lua-api/particle.lua
+++ docs/lua-api/particle.lua
@@ -3,6 +3,6 @@
 
 --- Spawns particles at the given location
 -- @param location location
--- @param effect effect as string
+-- @param effect effect as string. Can be every value of https://hub.spigotmc.org/javadocs/spigot/org/bukkit/Effect.html
 -- @param options options table with the fields count, speed, etc.
 function spawn(location, effect, options)
