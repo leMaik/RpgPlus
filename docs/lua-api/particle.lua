571f2f2f710ba5eb5166dc1127b685b609fde5cb
Johannes Pollitt <johannes@pollitt.de>
2016-08-05 22:40:39 +0200

    Add luadoc for the particles module.

---
+++ docs/lua-api/particle.lua
@@ -0,0 +1,8 @@
+--- Provides functions to spawn particles.
+-- @module rpgplus.particle
+
+--- Spawns particles at the given location
+-- @param location location
+-- @param effect effect as string
+-- @param options options table with the fields count, speed, etc.
+function spawn(location, effect, options)
