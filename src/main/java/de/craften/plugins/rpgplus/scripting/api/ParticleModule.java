09db59a7b0e9e9c7b1c83a5c44298dd4e6604836
Johannes Pollitt <johannes@pollitt.de>
2016-08-05 22:18:40 +0200

    Use the lua annotations to register the functions.

--- src/main/java/de/craften/plugins/rpgplus/scripting/api/ParticleModule.java
+++ src/main/java/de/craften/plugins/rpgplus/scripting/api/ParticleModule.java
@@ -5,46 +5,48 @@ import org.bukkit.Location;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
-import org.luaj.vm2.lib.VarArgFunction;
 
 import de.craften.plugins.rpgplus.RpgPlus;
 import de.craften.plugins.rpgplus.scripting.ScriptingModule;
 import de.craften.plugins.rpgplus.scripting.util.ScriptUtil;
+import de.craften.plugins.rpgplus.scripting.util.luaify.LuaFunction;
+import de.craften.plugins.rpgplus.scripting.util.luaify.Luaify;
 
-public class ParticleModule extends LuaTable implements ScriptingModule{
+public class ParticleModule extends LuaTable implements ScriptingModule {
 
 	public ParticleModule(final RpgPlus plugin) {
-		set("spawn", new VarArgFunction() {
-			
-			@Override
-            public Varargs invoke(Varargs varargs) {
-                
-				Location loc = ScriptUtil.getLocation(varargs.arg(1));
-				
-				Effect effect = Effect.valueOf(varargs.arg(2).tojstring().toUpperCase());
-				
-				int id = varargs.toint(3);
-				
-				int data = varargs.toint(4);
-				
-				float offsetX = (float) varargs.optdouble(5, 0);
-				float offsetY = (float) varargs.optdouble(6, 0);
-				float offsetZ = (float) varargs.optdouble(7, 0);
-				
-				float speed = (float) varargs.optdouble(8, 0);
-				
-				int particleCount = varargs.optint(9, 1);
-				
-				int radius = varargs.optint(10, 100);
-				
-				loc.getWorld().spigot().playEffect(loc, effect, id, data, offsetX, offsetY, offsetZ, speed, particleCount, radius);
-				
-                return LuaValue.NIL;
-            }
-			
-		});
+		Luaify.convertInPlace(this);
 	}
-	
+
+	@LuaFunction("spawn")
+	public void spawn(Varargs varargs) {
+
+		Location loc = ScriptUtil.getLocation(varargs.arg(1));
+
+		Effect effect = Effect
+				.valueOf(varargs.arg(2).tojstring().toUpperCase());
+
+		int id = varargs.toint(3);
+
+		int data = varargs.toint(4);
+
+		float offsetX = (float) varargs.optdouble(5, 0);
+		float offsetY = (float) varargs.optdouble(6, 0);
+		float offsetZ = (float) varargs.optdouble(7, 0);
+
+		float speed = (float) varargs.optdouble(8, 0);
+
+		int particleCount = varargs.optint(9, 1);
+
+		int radius = varargs.optint(10, 100);
+
+		loc.getWorld()
+				.spigot()
+				.playEffect(loc, effect, id, data, offsetX, offsetY, offsetZ,
+						speed, particleCount, radius);
+
+	}
+
 	@Override
 	public LuaValue getModule() {
 		return this;
@@ -52,8 +54,7 @@ public class ParticleModule extends LuaTable implements ScriptingModule{
 
 	@Override
 	public void reset() {
-		//nothing to do here
+		// nothing to do here
 	}
-
 
 }
